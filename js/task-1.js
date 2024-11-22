22967912db69d844ab3c29d979deb6c1809c812a
Sofi Dobriak <sofi.dobriak@gmail.com>
2024-11-22 20:50:54 +0200

    Finish all tasks

--- js/task-1.js
+++ js/task-1.js
@@ -3,17 +3,26 @@
 // ===============================================
 
 console.log('===== Task 1 ======');
-console.log(' ');
 
 // ===============================================
 
-function slugify(title) {
-    let slug = title.split(' ');
+function isEnoughCapacity(products, containerSize) {
+    let productCount = Object.values(products);
 
-    return slug.join('-').toLowerCase();
+    let sum = 0;
+
+    for (let product of productCount) {
+        sum += product;
+    }
+
+    if (sum < containerSize) {
+        return true;
+    }
+
+    return false;
 }
 
-console.log(slugify('Arrays for beginners')); // "arrays-for-beginners"
-console.log(slugify('English for developer')); // "english-for-developer"
-console.log(slugify('Ten secrets of JavaScript')); // "ten-secrets-of-javascript"
-console.log(slugify('How to become a JUNIOR developer in TWO WEEKS')); // "how-to-become-a-junior-developer-in-two-weeks"
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // false
